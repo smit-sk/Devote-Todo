4e925ad45da4e6bb5724424b60a9568f09a473be
smitkumar.khokhariya <smitkumar.khokhariya@corsameets.com>
2024-03-26 18:01:38 -0400

    blur effect on both theme

---
+++ Devote-Todo/View/CheckBoxStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CheckBoxStyle.swift
+//  Devote-Todo
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-25.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckBoxStyle()
+}
