60a76612796a5cfd54c1adb40ac7598406015f44
smitkumar.khokhariya <smitkumar.khokhariya@corsameets.com>
2024-03-26 21:36:30 -0400

    custom-app-widget for home screen

---
+++ Devote-Todo/Utility/SoundPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  SoundPlayer.swift
+//  Devote-Todo
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-26.
+//
+
+import SwiftUI
+
+struct SoundPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SoundPlayer()
+}
---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
